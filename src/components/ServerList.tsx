c913b768802e8954dfb3d295649f60eeb54b6ab0
paring <paring@pikokr.dev>
2023-11-19 14:56:19 +0900

    feat: edit server

--- src/components/ServerList.tsx
+++ src/components/ServerList.tsx
@@ -4,6 +4,7 @@ import React, { useState } from 'react'
 import { Server } from '@/utils/servers'
 import {
 	Button,
+	Input,
 	Modal,
 	ModalBody,
 	ModalContent,
@@ -18,7 +19,7 @@ import {
 	TableRow,
 	Tooltip,
 } from '@nextui-org/react'
-import { TbArrowDown, TbTrash } from 'react-icons/tb'
+import { TbArrowDown, TbEdit, TbTarget, TbTrash } from 'react-icons/tb'
 import { addServer, removeServer } from '@/actions'
 import { toast } from 'react-toastify'
 import { useRouter } from 'next/navigation'
@@ -27,9 +28,15 @@ export const ServerList: React.FC<{ servers: Server[] }> = ({ servers }) => {
 	const [serverToDelete, setServerToDelete] = React.useState<Server | null>(
 		null,
 	)
+	const [serverToEdit, setServerToEdit] = React.useState<Server | null>(null)
 	const [processing, setProcessing] = useState(false)
+	const [backend, setBackend] = React.useState('')
 
 	const router = useRouter()
+
+	React.useEffect(() => {
+		setBackend(serverToEdit?.backend ?? '')
+	}, [serverToEdit])
 
 	return (
 		<>
@@ -85,6 +92,64 @@ export const ServerList: React.FC<{ servers: Server[] }> = ({ servers }) => {
 					)}
 				</ModalContent>
 			</Modal>
+			<Modal isOpen={!!serverToEdit} onClose={() => setServerToEdit(null)}>
+				<ModalContent>
+					{(onClose) => (
+						<form
+							onSubmit={async (e) => {
+								e.preventDefault()
+
+								try {
+									setProcessing(true)
+									await addServer(serverToEdit!.hostname, backend)
+									router.refresh()
+									onClose()
+								} catch (e) {
+									if (e instanceof Error) {
+										toast.error(e.message)
+									}
+									throw e
+								} finally {
+									setProcessing(false)
+								}
+							}}
+						>
+							<ModalHeader>Edit server</ModalHeader>
+							<ModalBody>
+								Editing server:
+								<div className="flex gap-4 items-center flex-col">
+									<Snippet symbol="" color="primary" className="w-full">
+										{serverToEdit?.hostname}
+									</Snippet>
+									<Input
+										isRequired
+										disabled={processing}
+										label="Backend address"
+										endContent={<TbTarget />}
+										placeholder="127.0.0.1:12345"
+										variant="bordered"
+										value={backend}
+										onChange={(e) => setBackend(e.target.value)}
+									/>
+								</div>
+							</ModalBody>
+							<ModalFooter>
+								<Button
+									isDisabled={processing}
+									color="danger"
+									variant="flat"
+									onPress={onClose}
+								>
+									Close
+								</Button>
+								<Button isLoading={processing} color="primary" type="submit">
+									Edit server
+								</Button>
+							</ModalFooter>
+						</form>
+					)}
+				</ModalContent>
+			</Modal>
 			<Table className="mt-4" aria-label="Servers">
 				<TableHeader>
 					<TableColumn>Hostname</TableColumn>
@@ -109,7 +174,15 @@ export const ServerList: React.FC<{ servers: Server[] }> = ({ servers }) => {
 								</div>
 							</TableCell>
 							<TableCell className="pb-0">
-								<div className="flex justify-center">
+								<div className="flex justify-center gap-4">
+									<Tooltip content="Edit">
+										<span
+											className="text-lg transition-opacity opacity-75 hover:opacity-100 cursor-pointer active"
+											onClick={() => setServerToEdit(x)}
+										>
+											<TbEdit />
+										</span>
+									</Tooltip>
 									<Tooltip content="Delete">
 										<span
 											className="text-lg transition-colors text-red-400 hover:text-red-500 cursor-pointer active"
